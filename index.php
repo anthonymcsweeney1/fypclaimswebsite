bfcf728b0e5d851704abc512011bba8f75382478
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2020-11-12 15:19:29 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
-<?php
-session_start();
-?>
+
 <!DOCTYPE html>
 <html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <body>
+     <form action="login.php" method="post">
+     	<h2>LOGIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>Username</label>
+     	<input type="text" name="uname" placeholder="Username"><br>
 
-<?php
-// to change a session variable, just overwrite it
-$_SESSION["favcolor"] = "yellow";
-print_r($_SESSION);
-?>
+     	<label>Password</label>
+     	<input type="password" name="password" placeholder="Password"><br>
 
+     	<button type="submit">Login</button>
+
+
+     </form>
 </body>
-</html>
+</html
