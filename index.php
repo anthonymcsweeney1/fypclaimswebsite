fbe844a0dda2bcdf3b4f7d680881ff6c009ed3ed
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2020-11-12 15:33:57 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,76 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-     <form action="login.php" method="post">
-     	<h2>LOGIN</h2>
-     	<?php if (isset($_GET['error'])) { ?>
-     		<p class="error"><?php echo $_GET['error']; ?></p>
-     	<?php } ?>
-     	<label>Username</label>
-     	<input type="text" name="uname" placeholder="Username"><br>
-
-     	<label>Password</label>
-     	<input type="password" name="password" placeholder="Password"><br>
-
-     	<button type="submit">Login</button>
-
-
-     </form>
-</body>
-</html
+<?php  
+ session_start();  
+ $host = "localhost";  
+ $username = "root";  
+ $password = "";  
+ $database = "testing";  
+ $message = "";  
+ try  
+ {  
+      $connect = new PDO("sqlsrv:server = tcp:fyserver1.database.windows.net,1433; Database = FYP", "azureBIS", "LokiKovex3!");
+      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+      if(isset($_POST["login"]))  
+      {  
+           if(empty($_POST["username"]) || empty($_POST["password"]))  
+           {  
+                $message = '<label>All fields are required</label>';  
+           }  
+           else  
+           {  
+                $query = "SELECT * FROM users WHERE username = :username AND password = :password";  
+                $statement = $connect->prepare($query);  
+                $statement->execute(  
+                     array(  
+                          'username'     =>     $_POST["username"],  
+                          'password'     =>     $_POST["password"]  
+                     )  
+                );  
+                $count = $statement->rowCount();  
+                if($count > 0)  
+                {  
+                     $_SESSION["username"] = $_POST["username"];  
+                     header("location:login_success.php");  
+                }  
+                else  
+                {  
+                     $message = '<label>Wrong Data</label>';  
+                }  
+           }  
+      }  
+ }  
+ catch(PDOException $error)  
+ {  
+      $message = $error->getMessage();  
+ }  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Webslesson Tutorial | PHP Login Script using PDO</title>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+      </head>  
+      <body>  
+           <br />  
+           <div class="container" style="width:500px;">  
+                <?php  
+                if(isset($message))  
+                {  
+                     echo '<label class="text-danger">'.$message.'</label>';  
+                }  
+                ?>  
+                <h3 align="">PHP Login Script using PDO</h3><br />  
+                <form method="post">  
+                     <label>Username</label>  
+                     <input type="text" name="username" class="form-control" />  
+                     <br />  
+                     <label>Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />  
+                     <input type="submit" name="login" class="btn btn-info" value="Login" />  
+                </form>  
+           </div>  
+           <br />  
+      </body>  
+ </html>
